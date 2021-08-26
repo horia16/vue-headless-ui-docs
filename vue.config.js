aa45cb936547a64bd481881664d3822c9998b439
Horia Rotaru <horia.rotaru1@gmail.com>
2021-08-26 20:41:41 +0100

    Loaded tailwind styles

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const tailwindcss = require('tailwindcss'); 
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/vue-headless-ui-docs/'
-  : '/'
+  : '/',
+  css: {
+    loaderOptions: {
+        postcss: {
+            plugins: [
+                tailwindcss,
+                autoprefixer,
+            ],
+        },
+    },
+},
   }
